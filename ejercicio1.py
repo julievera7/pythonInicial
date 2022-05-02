b82226892b805f45b984e1874424b1bb760e5b6a
Aliet <julievera7@gmail.com>
2022-05-02 12:24:15 -0300

    Create ejercicio1.py
    Primer ejercicio del curso Python Báscico Open BootCamp

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+#Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+edad = int(input("Indique su edad: "))
+
+print(edad)
+
+if edad < 18 :
+	print("Es menor de edad")
+else :
+	print("Es mayor de edad")
