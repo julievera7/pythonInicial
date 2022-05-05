91ede242457b4e3b4840485e193248a5198743b0
aliet <julievera7@gmail.com>
2022-05-05 12:57:40 -0300

    Ejercicio 9

---
+++ ejercicio9/main.py
@@ -0,0 +1,14 @@
+import modulo
+
+def main():
+	rsuma = modulo.suma(10,2)
+	print("Resultado de la suma: ", rsuma)
+	rresta = modulo.resta(10,2)
+	print("Resultado de la resta: ", rresta)
+	rproducto = modulo.producto(10,2)
+	print("Resultado de la multiplicación: ", rproducto)
+	rdivision = modulo.division(10,2)
+	print("Resultado de la división: ", rdivision)
+	
+if __name__ == '__main__':
+	main()
